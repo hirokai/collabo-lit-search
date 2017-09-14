182183a01a69c4f5672c276c029627406533ae92
Hiroyuki Kai <fullerenec84@gmail.com>
2017-09-14 11:37:25 +0900

    Parent-child of tabs

--- chrome_extension/eventPage.js
+++ chrome_extension/eventPage.js
@@ -5,35 +5,70 @@
 const username = 'Kai';
 
 const watchTarget = (url) => {
-  if (url.indexOf("https://www.nature.com/") == 0) return true;
+  const urls = ["https://www.nature.com/",'http://pubs.acs.org/doi/'];
+  if(_.find(urls,(u) => {return url.indexOf(u) == 0;})){
+    return true;
+  }else{
+    return false;
+  }
 };
+
+chrome.tabs.onCreated.addListener((tab) => {
+  chrome.tabs.get(tab.openerTabId,(parent) => {
+    console.log('Opened from: ' + parent.title);
+  });
+});
+
+var url_history = {};
+var title_list = {};
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   // console.debug(changeInfo);
   if (watchTarget(tab.url)) {
     var str = '';
     if (changeInfo.title) {
+      title_list[tab.url] = tab.title;
       str += 'Loading: ' + changeInfo.title + ' ';
-      sendLogTitle(tab.url, changeInfo.title)
+      if(tab.openerTabId){
+        chrome.tabs.get(tab.openerTabId,(parent) => {
+          sendLogTitle(tab.url, parent.url, tab.title, (res) => {
+            console.log(res);
+          });
+        });
+      }else{
+        sendLogTitle(tab.url, null, tab.title, (res) => {
+          console.log(res);
+        });
+      }
     } else if (changeInfo.status == 'loading') {
-      sendLogUrl(tab.url, (res) => {
-        console.log(res);
-      });
+      if(url_history[tab.id]){
+        url_history[tab.id].push(tab.url);
+      }else{
+        url_history[tab.id] = [tab.url];
+      }
+
     } else if (changeInfo.status == 'complete') {
       str += 'Complete: ';
-      console.log(tab);
+      console.log(url_history);
     }
     str += tab.url;
     console.log(str);
   }
 });
 
-function sendLogUrl(url, callback) {
-  $.post('http://localhost:3000/add_history', {user: username, url: url}, callback);
-}
+chrome.tabs.onActivated.addListener((activeInfo) => {
+  const h = url_history[activeInfo.tabId];
+  console.log(url_history,activeInfo,h);
+    if(h){
+      const title = title_list[h[h.length-1]];
+      $.post('http://localhost:3000/add_history', {user: username, action: 'change_tab', title: title}, (res) => {
+        console.log(res);
+      });
+    }
+});
 
-function sendLogTitle(url, title, callback) {
-  $.post('http://localhost:3000/add_history', {user: username, url: url, title: title}, callback);
+function sendLogTitle(url, parent, title, callback) {
+  $.post('http://localhost:3000/add_history', {user: username, action: 'browse', url: url, title: title}, callback);
 }
 
 pubnub = new PubNub({
