41f4f41b4ab78e5db9035cf69d7a058521bbcdc3
Nebuta <nebuta.office@gmail.com>
2017-09-19 07:14:35 +0900

    Remove unnecessary code. Add username storage.

--- chrome_extension/popup.js
+++ chrome_extension/popup.js
@@ -88,33 +88,21 @@ function getImageUrl(searchTerm, callback, errorCallback) {
   x.send();
 }
 
+$(() => {
+  chrome.storage.sync.get('username',function(items){
+    $('#set_username').val(items.username);
+  });
+  $('#set_username').click((ev) => {
+    const n = $('#username').val();
+    console.log(n);
+    chrome.storage.sync.set({username: n});
+  })
+});
+
 function renderStatus(statusText) {
   document.getElementById('status').textContent = statusText;
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-        // chrome.notifications.create('1',{type: 'basic', iconUrl: 'icon.png', title: 'Cols', message: tab.title});
-
-  getCurrentTabUrl(function(url) {
-    // Put the image URL in Google search.
-    renderStatus('Performing Google Image search for ' + url);
-
-    getImageUrl(url, function(imageUrl, width, height) {
-
-      renderStatus('Search term: ' + url + '\n' +
-          'Google image search result: ' + imageUrl);
-      var imageResult = document.getElementById('image-result');
-      // Explicitly set the width/height to minimize the number of reflows. For
-      // a single image, this does not matter, but if you're going to embed
-      // multiple external images in your page, then the absence of width/height
-      // attributes causes the popup to resize multiple times.
-      imageResult.width = width;
-      imageResult.height = height;
-      imageResult.src = imageUrl;
-      imageResult.hidden = false;
-
-    }, function(errorMessage) {
-      renderStatus('Cannot display image. ' + errorMessage);
-    });
-  });
+  renderStatus('Hey');
 });
